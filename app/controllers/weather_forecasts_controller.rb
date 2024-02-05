be68ae1b5e3b7c38263d7a97f991b87bbbf3185d
Michael Romano <micha7a.com@gmail.com>
2024-02-05 19:01:31 -0400

    Fix permit vs require

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -5,7 +5,7 @@ class WeatherForecastsController < ApplicationController
   end
 
   def create
-    @forecast = WeatherForecastService.new(weather_forecast_params[:address]).call
+    @forecast = WeatherForecastService.new(weather_forecast_params).call
     if @forecast.periods.present?
       data_state = @forecast.data_state
       flash.now[data_state == :cached ? :warning : :success] = "This data is #{data_state}."
@@ -19,6 +19,6 @@ class WeatherForecastsController < ApplicationController
   private
 
   def weather_forecast_params
-    params.permit(:address)
+    params.require(:address)
   end
 end
