097adc5a2be080d53c2707289e5d194b5376e597
Michael Romano <micha7a.com@gmail.com>
2024-02-07 17:37:06 -0400

    Move form into navbar, add dropdown to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def alert_class_for(flash_type)
+    {
+      notice: "alert-success",
+      erorr: "alert-danger"
+    }[flash_type.to_sym] || "alert-#{flash_type.to_s}"
+  end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.min.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
+pin "bootstrap", to: "bootstrap.bundle.min.js"
